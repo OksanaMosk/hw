e1142cea53eb0f93cee64904931e49a5ed34fb80
OksanaMosk <oks-m3@ukr.net>
2025-07-06 10:45:16 +0300

    hw1_2

--- src/Components/CoursesComponent.tsx
+++ src/Components/CoursesComponent.tsx
@@ -6,7 +6,7 @@ import {CourseComponent} from "./CourseComponent.tsx"
 export const CoursesComponent = () => {
     return (
         <ul className="flex flex-col gap-[40px] justify-center w-100 items-center mt-20
-mx-auto p-[20px] text-xl bg-sky-200 shadow-xl rounded-xl">
+mx-auto p-[20px] text-xl bg-lime-200 shadow-xl rounded-xl ">
             {coursesAndDurationArray.map((course: ICourseModal, index:number)=>{
                 return <CourseComponent course={course} key={index}/>
             })
