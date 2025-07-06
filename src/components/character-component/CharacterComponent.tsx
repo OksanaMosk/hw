685f96a3fc80f39eb59bee78f13c45341dad5566
OksanaMosk <oks-m3@ukr.net>
2025-07-06 13:56:53 +0300

    hw1_3

--- src/components/character-component/CharacterComponent.tsx
+++ src/components/character-component/CharacterComponent.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode} from 'react';
+import  {ReactNode} from 'react';
 import {ICharacter} from "../../models/ICharacter.ts";
 import "./CharacterComponent.css"
 
@@ -8,14 +8,22 @@ interface ICharacterComponentProps{
 }
 export const CharacterComponent= ({item, children}:ICharacterComponentProps ) => {
     return (
-      <li>
-        <h3>
-          {item.name} {item.surname}
-        </h3>
-        <img src={item.photo} alt={item.name} />
-        <p>{children}</p>
-      </li>
+        <li
+            className="
+    w-[600px] h-100 p-4 shadow-xl rounded-xl text-lime-800 bg-lime-300
+    flex flex-col justify-between
+    gap-[8px]
+    dark:text-gray-400 hover:text-green-950 hover:bg-lime-400
+    cursor-pointer transition duration-200 ease-in-out
+  "
+        >
+            <h3 className="text-xl font-bold self-start">
+                {item.name} {item.surname}
+            </h3>
+
+            <img src={item.photo} alt={item.name} className="max-h-48 object-contain" />
+
+            <p className="text-[12px] ">{children}</p>
+        </li>
     );
 };
-
-
--- src/components/family-component/FamilyComponent.tsx
+++ src/components/family-component/FamilyComponent.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+
 import {simpsons} from "../../data/arrays-2.ts";
-import {ICharacter} from "../models/ICharacter.ts";
+import {ICharacter} from "../../models/ICharacter.ts";
 import {CharacterComponent} from "../character-component/CharacterComponent.tsx";
 
 export const FamilyComponent = () => {
     return (
-        <ul>
+        <ul className="flex flex-wrap gap-[30px] justify-center w-5/6 items-center m-[30px]
+mx-auto ">
             {
                 simpsons.map((value:ICharacter,index:number )=>
                 <CharacterComponent key={index} item={value} >{value.info}</CharacterComponent>)
