e8c80ce9d16d8c3dd5dac65cb21a1c07ae7be2f9
OksanaMosk <oks-m3@ukr.net>
2025-07-07 20:24:46 +0300

    hw_2_2 props style

--- src/components/PostComponent.tsx
+++ src/components/PostComponent.tsx
@@ -1,15 +1,16 @@
 import {IPost} from "../models/PostModel.ts";
+import { FC } from 'react';
 
-interface IPostComponentProps {
+type PostComponentProps= {
   post: IPost;
 }
 
-const PostComponent = ({ post:{id, userId, title, body} }: IPostComponentProps) => {
+const PostComponent:FC<PostComponentProps>= ({ post:{id, userId, title, body} }) => {
   return <li className=" flex flex-col justify-between w-[400px] gap-1 h-60  text-[#fcfbfc] bg-[#93a368] p-7
    shadow-xl rounded-xl   hover:text-[#cadbb7] hover:bg-[#485936]
-    cursor-pointer transition duration-200 ease-in-out
+    cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105 hover:rotate-1 hover:shadow-2xl
   ">
-    <h3  className="flex justify-start w-full text-[14px] font-bold  ">{title}</h3>
+    <h3  className="flex w-full text-[14px] font-bold  ">{title}</h3>
 
  <div className="flex justify-between ">
    <p>ID: {id}</p>
