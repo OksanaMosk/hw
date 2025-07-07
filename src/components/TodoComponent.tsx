c4c6090707ad11241bcd135b165d0b855ecfd58c
OksanaMosk <oks-m3@ukr.net>
2025-07-07 20:17:22 +0300

    hw_2_1 props style

--- src/components/TodoComponent.tsx
+++ src/components/TodoComponent.tsx
@@ -1,20 +1,21 @@
 import {ITodoModel} from "../models/TodoModel.ts";
+import {FC} from "react";
 
-interface ITodoProps {
+type TodoProps= {
     todo:ITodoModel
 }
-export const TodoComponent = ({todo:{id,userId,title,completed}}:ITodoProps) => {
+export const TodoComponent:FC<TodoProps> = ({todo:{id,userId,title,completed}}) => {
     return (
         <li  className=" flex flex-col justify-between w-[400px] h-40  text-[#508068] bg-[#e4dedf] p-7
    shadow-xl rounded-xl   hover:text-[#445C4C] hover:bg-[#BFC9CA]
     cursor-pointer transition duration-200 ease-in-out
   ">
-            <h2 className="flex justify-start w-[100%] text-[18px] font-bold  ">Title {title}</h2>
+            <h2 className="flex  w-full text-[18px] font-bold  ">Title {title}</h2>
             <div  className="flex justify-between">
             <p>ID: {id}</p>
             <p>USERID: {userId}</p>
             </div>
-            <p className=" w-[100%] font-bold size-4 m-0 ">Completed Status: {completed.toString()}</p>
+            <p className=" w-full font-bold size-[16px] m-0 ">Completed Status: {completed.toString()}</p>
         </li>
     );
 };
